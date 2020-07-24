5bc071bc90cfd12340de5d335f8582dc53def8fe
kimkim2951 <ty53.k.k@gmail.com>
2020-07-24 19:59:11 +0900

    initial

---
+++ db/migrate/20200724102803_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.integer :user_id
+      t.integer :tweet_id
+      t.text :text
+      t.timestamps
+    end
+  end
+end
