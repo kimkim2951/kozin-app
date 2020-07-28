47801c7cacbaaf83953bb1ef45bd3006f1e84fa9
kimkim2951 <ty53.k.k@gmail.com>
2020-07-29 02:47:35 +0900

    DB変更 Haml変更

--- db/migrate/20200724102728_create_tweets.rb
+++ db/migrate/20200724102728_create_tweets.rb
@@ -1,9 +1,13 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :name
-      t.string :text
-      t.string :image
+      t.text "title"
+      t.text "text"
+      t.text "image"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.integer "user_id"
+      t.index ["text"], name: "index_tweets_on_text", length: 32
       t.timestamps
     end
   end
--- db/migrate/20200724102900_create_comments.rb
+++ db/migrate/20200724102900_create_comments.rb
@@ -1,9 +1,11 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :tweet_id
-      t.text :text
+      t.integer "user_id"
+      t.integer "tweet_id"
+      t.text "text"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
       t.timestamps
     end
   end
