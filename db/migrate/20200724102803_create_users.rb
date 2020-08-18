87bfaccb998d30b2a9a0bc6a89c61e172b09bc6f
kimkim2951 <ty53.k.k@gmail.com>
2020-08-19 02:20:46 +0900

    [fix]config/initialize、ユーザーDB修正

--- db/migrate/20200724102803_create_users.rb
+++ db/migrate/20200724102803_create_users.rb
@@ -7,9 +7,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string "email", default: "", null: false
       t.string "encrypted_password", default: "", null: false
       t.string "reset_password_token"
+      t.string "name"
       t.datetime "reset_password_sent_at"
       t.datetime "remember_created_at"
-      t.string "name"
       t.index ["email"], name: "index_users_on_email", unique: true
       t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,9 @@ ActiveRecord::Schema.define(version: 2020_07_29_093704) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
+    t.string "name"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
