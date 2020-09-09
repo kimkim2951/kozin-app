f391164093e62ee5b9daeb9fafe9a4cdc319ae40
kimkim2951 <ty53.k.k@gmail.com>
2020-09-10 01:29:02 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'kozin-app'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV[:access_key_id],
-      aws_secret_access_key: ENV[:secret_access_key],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'kozin-app'
