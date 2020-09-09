2f729bb84831aea03b086f0e4ed89df373f950d3
kimkim2951 <ty53.k.k@gmail.com>
2020-09-10 01:07:45 +0900

    [fix]

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'kozin-app'
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket80d'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kozin-app'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
