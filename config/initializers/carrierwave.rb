2c728d65750b693a962d259667bd06d2f87343ee
kimkim2951 <ty53.k.k@gmail.com>
2020-09-10 18:11:13 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,9 @@ CarrierWave.configure do |config|
     region:                 ENV['S3_REGION'],   
     aws_access_key_id:      ENV['S3_ACCESS_KEY_ID'],
     aws_secret_access_key:  ENV['S3_SECRET_ACCESS_KEY']
+    path_style: true
   }
   config.fog_directory     =  ENV['S3_BUCKET']
+  config.fog_public     = true
 end
 # end
