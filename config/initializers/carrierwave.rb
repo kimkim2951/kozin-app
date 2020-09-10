70a2773032c414818df611a1addd3f979a5c316d
kimkim2951 <ty53.k.k@gmail.com>
2020-09-10 17:06:20 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,22 +2,9 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#     region: 'ap-northeast-1'
-#   }
-
-#   config.fog_directory  = 'kozin-app'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kozin-app'
-# end
-
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
     config.fog_credentials = {
       # Amazon S3用の設定
       provider:               'AWS',
@@ -26,5 +13,8 @@ if Rails.env.production?
       aws_secret_access_key:  ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
