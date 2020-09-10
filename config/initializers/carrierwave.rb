520f535916b54c7f1a01e68dae725edd2c03793c
kimkim2951 <ty53.k.k@gmail.com>
2020-09-10 20:07:19 +0900

    [fix]トップ画面imageパス修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,22 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'kozin-app'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:aws_access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:sws_secret_access_key],
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory  = 'kozin-app'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kozin-app'
-  else
-    config.storage :file # 開発環境:public/uploades下に保存
-    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'kozin-app'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kozin-app'
 end
